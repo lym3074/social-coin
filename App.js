7fb9a08c7f01a7d139f0e470d492f12d2b87bc91
Yongmin <qwelllo@naver.com>
2023-10-02 14:08:09 +0900

    [ ~ 6.3] IOS Firebase 설정

--- App.js
+++ App.js
@@ -1,21 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
+import auth from '@react-native-firebase/auth';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  useEffect(()=> {console.log(auth().currentUser)},[])
+  return null;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- ios/socialcoin/AppDelegate.mm
+++ ios/socialcoin/AppDelegate.mm
@@ -2,11 +2,16 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTLinkingManager.h>
+#import <Firebase.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+
+  if([FIRApp defaultApp] == nil)
+    [FIRApp configure];
+
   self.moduleName = @"main";
 
   // You can add your custom initial props in the dictionary below.
