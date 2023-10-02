8666e8f2ab35e86d43bfcf5756e34f726e593bef
Yongmin <qwelllo@naver.com>
2023-10-02 19:15:46 +0900

    [6.10] 빅토리 차트 다운, axios 받아오기

--- Screens/Detail.js
+++ Screens/Detail.js
@@ -1,10 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components/native";
 import { Icon } from "../components/Coin";
 import { useQuery } from "react-query";
 import { history, info } from "../api";
+import { BLACK_COLOR } from "../colors";
 
-const Container = styled.View``;
+const Container = styled.ScrollView`
+    background-color: ${BLACK_COLOR}
+`;
 
 const Detail = ({navigation, route: {params: {symbol, id}}}) => {
     useEffect(()=> {
@@ -13,10 +16,19 @@ const Detail = ({navigation, route: {params: {symbol, id}}}) => {
         });
     },[]);
 
-    const {isLoading: infoLoading, data: infoData} = useQuery(["coinInfo", id], info)
-    const {isLoading: historyLoading, historyData} = useQuery(["coinHistory",id], history)
+    const {isLoading: infoLoading, data: infoData} = useQuery("coinInfo", () => info(id)) // 함수형은 캐시를 잃을 수 있음
+    const {isLoading: historyLoading, data: historyData} = useQuery(["coinHistory",id], history)
     
-    console.log(infoData)
+    const [victoryData, setVictoryData] = useState([]);
+
+    useEffect(()=>{
+        if(historyData) {
+            const axios = historyData.map(price => ({x: new Date(price.timestamp).getTime(), y: price.price}))
+            setVictoryData(axios);
+        }
+    },[historyData]);
+
+    console.log(victoryData)
     return <Container></Container>
 };
 
--- api.js
+++ api.js
@@ -3,5 +3,5 @@ const COINS_URL = `${BASE_URL}/coins`;
 
 export const coins = () => fetch(COINS_URL).then(res => res.json());
 
-export const info = ({queryKey}) => fetch(`${COINS_URL}/${queryKey[1]}`).then(res => res.json())
-export const history = ({queryKey}) => fetch(`${BASE_URL}/tickers/${queryKey[1]}/historical?start=${new Date().toISOString().split("T")[0]}&interval=30m`).then(res => res.json())
+export const info = (coinId) => fetch(`${COINS_URL}/${coinId}`).then(res => res.json())
+export const history = ({queryKey}) => fetch(`${BASE_URL}tickers/${queryKey[1]}/historical?start=${new Date().toISOString().split("T")[0]}&interval=2h`).then(res => res.json())
